b9c7c4cf001906c98f17278e6300fb333f0907f6
Anton Netsvetaev <toni.neczvetaev.06@bk.ru>
2026-07-21 21:57:36 +0300

    feat: написал тест SetLimitTests.CreateLimit_WhenPartnerNotFound_ReturnsNotFound

--- Homeworks/04 Добавление юнит тестов/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetLimitTests.cs
+++ Homeworks/04 Добавление юнит тестов/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetLimitTests.cs
@@ -1,10 +1,47 @@
+using AwesomeAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.WebHost.Controllers;
+using PromoCodeFactory.WebHost.Models.Partners;
+
 namespace PromoCodeFactory.UnitTests.WebHost.Controllers.Partners;
 
 public class SetLimitTests
 {
+    private readonly Mock<IRepository<Partner>> _partnersRepositoryMock;
+    private readonly Mock<IRepository<PartnerPromoCodeLimit>> _partnerLimitsRepositoryMock;
+    private readonly PartnersController _sut;
+
+    public SetLimitTests()
+    {
+        _partnersRepositoryMock = new Mock<IRepository<Partner>>();
+        _partnerLimitsRepositoryMock = new Mock<IRepository<PartnerPromoCodeLimit>>();
+        _sut = new PartnersController(_partnersRepositoryMock.Object, _partnerLimitsRepositoryMock.Object);
+    }
+
     [Fact]
     public async Task CreateLimit_WhenPartnerNotFound_ReturnsNotFound()
     {
+        // Arrange
+        var partnerId = Guid.NewGuid();
+        var request = new PartnerPromoCodeLimitCreateRequest(EndAt: DateTime.UtcNow.AddDays(2), Limit: 5);
+        _partnersRepositoryMock
+            .Setup(r => r.GetById(partnerId, true, It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Partner?)null);
+
+        // Act
+        var result = await _sut.CreateLimit(partnerId, request, CancellationToken.None);
+
+        // Assert
+        result.Should().BeOfType<ActionResult<PartnerPromoCodeLimitResponse>>();
+        result.Result.Should().NotBeNull();
+        result.Result.Should().BeOfType<NotFoundObjectResult>();
+        var notFoundResult = (NotFoundObjectResult)result.Result;
+        notFoundResult.Value.Should().BeOfType<ProblemDetails>();
+        var problemDetails = (ProblemDetails)notFoundResult.Value;
+        problemDetails.Title.Should().Be("Partner not found");
     }
 
     [Fact]
