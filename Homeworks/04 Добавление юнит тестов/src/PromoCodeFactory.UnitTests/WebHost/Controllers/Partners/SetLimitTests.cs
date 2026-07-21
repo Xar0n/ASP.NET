7001349a2520c3e50246bf268b0de3f18fbc25c7
Anton Netsvetaev <toni.neczvetaev.06@bk.ru>
2026-07-21 22:34:42 +0300

    feat: реализовал тест SetLimitTests.CreateLimit_WhenPartnerBlocked_ReturnsUnprocessableEntity

--- Homeworks/04 Добавление юнит тестов/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetLimitTests.cs
+++ Homeworks/04 Добавление юнит тестов/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetLimitTests.cs
@@ -2,9 +2,11 @@ using AwesomeAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.Administration;
 using PromoCodeFactory.Core.Domain.PromoCodeManagement;
 using PromoCodeFactory.WebHost.Controllers;
 using PromoCodeFactory.WebHost.Models.Partners;
+using Soenneker.Utils.AutoBogus;
 
 namespace PromoCodeFactory.UnitTests.WebHost.Controllers.Partners;
 
@@ -47,6 +49,26 @@ public class SetLimitTests
     [Fact]
     public async Task CreateLimit_WhenPartnerBlocked_ReturnsUnprocessableEntity()
     {
+        // Arrange
+        var partnerId = Guid.NewGuid();
+        var request = new PartnerPromoCodeLimitCreateRequest(EndAt: DateTime.UtcNow.AddDays(2), Limit: 5);
+        var partner = CreatePartner(partnerId, false);
+
+        _partnersRepositoryMock
+            .Setup(r => r.GetById(partnerId, true, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(partner);
+
+        // Act
+        var actionResult = await _sut.CreateLimit(partnerId, request, CancellationToken.None);
+
+        // Assert
+        actionResult.Should().BeOfType<ActionResult<PartnerPromoCodeLimitResponse>>();
+        actionResult.Result.Should().NotBeNull();
+        actionResult.Result.Should().BeOfType<UnprocessableEntityObjectResult>();
+        var unprocessableEntityObjectResult = (UnprocessableEntityObjectResult)actionResult.Result;
+        unprocessableEntityObjectResult.Value.Should().BeOfType<ProblemDetails>();
+        var problemDetails = (ProblemDetails)unprocessableEntityObjectResult.Value;
+        problemDetails.Title.Should().Be("Partner blocked");
     }
 
     [Fact]
@@ -62,5 +84,28 @@ public class SetLimitTests
     [Fact]
     public async Task CreateLimit_WhenUpdateThrowsEntityNotFoundException_ReturnsNotFound()
     {
+    }
+
+    private static Partner CreatePartner(
+        Guid partnerId,
+        bool isActive,
+        DateTimeOffset? canceledAt = null)
+    {
+        var role = new AutoFaker<Role>()
+            .RuleFor(r => r.Id, _ => Guid.NewGuid())
+            .Generate();
+
+        var employee = new AutoFaker<Employee>()
+            .RuleFor(e => e.Id, _ => Guid.NewGuid())
+            .RuleFor(e => e.Role, role)
+            .Generate();
+
+        var partner = new AutoFaker<Partner>()
+            .RuleFor(p => p.Id, _ => partnerId)
+            .RuleFor(p => p.IsActive, _ => isActive)
+            .RuleFor(p => p.Manager, employee)
+            .Generate();
+
+        return partner;
     }
 }
